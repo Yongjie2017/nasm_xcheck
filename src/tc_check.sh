4def35a78f5b0ff2767f30386bf805bf553112e4
Sheng, Yongjie <yongjie.sheng@intel.com>
2025-09-11 10:42:42 +0100

    revive compare between 2.16.03 and 3.00.rc3

--- src/tc_check.sh
+++ src/tc_check.sh
@@ -16,28 +16,28 @@ nasm_insns=$(ls | sed 's/^test_//' | sed 's/_nasm\.asm//' | sort -u)
 popd
 
 # compare output between nasm 2.16.03 and 3.00.rc3
-# echo "Comparing output between nasm 2.16.03 and 3.00.rc3"
-# for insn in $nasm_insns
-# do
-# 	if [ -f ${output_3_00_rc3}/test_${insn}_nasm.asm.o -a -f ${output_2_16_03}/test_${insn}_nasm.asm.o ]; then
-# 		echo -n "$insn ... "
-# 		objdump -d ${output_2_16_03}/test_${insn}_nasm.asm.o | tail -n +4 > /tmp/2_16_03.dump
-# 		objdump -d ${output_3_00_rc3}/test_${insn}_nasm.asm.o | tail -n +4 > /tmp/3_00_rc3.dump
+echo "Comparing output between nasm 2.16.03 and 3.00.rc3"
+for insn in $nasm_insns
+do
+	if [ -f ${output_3_00_rc3}/test_${insn}_nasm.asm.o -a -f ${output_2_16_03}/test_${insn}_nasm.asm.o ]; then
+		echo -n "$insn ... "
+		objdump -d ${output_2_16_03}/test_${insn}_nasm.asm.o | tail -n +4 > /tmp/2_16_03.dump
+		objdump -d ${output_3_00_rc3}/test_${insn}_nasm.asm.o | tail -n +4 > /tmp/3_00_rc3.dump
 
-# 		# applying a work around on prefix group3/4 order, if doesn't match
-# 		if ! diff /tmp/2_16_03.dump /tmp/3_00_rc3.dump >/dev/null; then
-# 			sed -i 's/:\t67 66 /:\t66 67 /' /tmp/3_00_rc3.dump
-# 		fi
+		# applying a work around on prefix group3/4 order, if doesn't match
+		if ! diff /tmp/2_16_03.dump /tmp/3_00_rc3.dump >/dev/null; then
+			sed -i 's/:\t67 66 /:\t66 67 /' /tmp/3_00_rc3.dump
+		fi
 
-# 		if ! diff /tmp/2_16_03.dump /tmp/3_00_rc3.dump >/dev/null; then
-# 			echo ""
-# 			diff /tmp/2_16_03.dump /tmp/3_00_rc3.dump
-# 			cat $src_nasm/test_${insn}_nasm.asm
-# 		else
-# 			echo "Done"
-# 		fi
-# 	fi
-# done
+		if ! diff /tmp/2_16_03.dump /tmp/3_00_rc3.dump >/dev/null; then
+			echo ""
+			diff /tmp/2_16_03.dump /tmp/3_00_rc3.dump
+			cat $src_nasm/test_${insn}_nasm.asm
+		else
+			echo "Done"
+		fi
+	fi
+done
 
 # compare output between nasm 3.00.rc3 and gas
 echo "Comparing output between nasm gas and 3.00.rc3"
